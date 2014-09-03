16aaac218dbc002375e65668e9360daff0da41a4
Tobias Weichart <weichart@hallowelt.biz>
2014-09-03 16:27:56 +0200

    several skin changes according to HW#2014082710000113 note #9
    Change-Id: Ibc7cf842d87d13e014426002cd05336ce940ebb5

--- resources/components/skin.menuTop.js
+++ resources/components/skin.menuTop.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$("#bs-button-user div.bs-userminiprofile, #bs-personal-name").click(function(e) {
 		e.preventDefault();
+		var event = e;
 		$('#bs-personal-menu-container').fadeToggle(200);
 		//needs to be done here, bs-personal-menu-top got a width of 0px before the click due to display none
 		if ($("#bs-button-user div.bs-personal-menu-top").css('margin-left') === "0px"){
@@ -10,6 +11,13 @@ $(document).ready(function(){
 			$("#bs-button-user div.bs-personal-menu-top").css("margin-left", marginLeft);
 		}
 
+	});
+	$(document).click(function(e){
+		if ($(e.target).attr("id") !== "bs-personal-name" && $(e.target).parent().parent().parent().attr('id') !== "bs-button-user" && $(e.target).parent().attr('id') !== 'bs-cactions-button'){
+			$('#bs-personal-menu-container').fadeOut(200);
+			$('#bs-flyout').fadeOut(200);
+			$("#bs-cactions-button > a").removeClass("open");
+		}
 	});
 	var menuTop = $("<div>");
 	menuTop.addClass("bs-personal-menu-top");
